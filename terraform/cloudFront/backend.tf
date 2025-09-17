e133b9690fcf6f241001c3257887f88b3e749e7d
sabahat21 <faheemsa@sheridancollege.ca>
2025-09-17 11:04:17 -0400

    Update backend.tf

--- terraform/cloudFront/backend.tf
+++ terraform/cloudFront/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "SanskritDeployment" 
 
     workspaces { 
-      name = "gameshow-cloudFront" 
+      name = "CloudFront" 
     } 
   } 
 }
