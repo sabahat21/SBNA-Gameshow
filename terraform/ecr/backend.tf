3668ecb8d14780544c710522a279dbb5913a3fb4
sabahat21 <faheemsa@sheridancollege.ca>
2025-09-17 10:58:58 -0400

    Update backend.tf

--- terraform/ecr/backend.tf
+++ terraform/ecr/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "SanskritDeployment" 
 
     workspaces { 
-      name = "gameshow-ecr" 
+      name = "BackendECR" 
     } 
   } 
 }
